2669a32ed12326ad59de09e9f1ddbc46b34e6e4e
Gabarros <gabarros17@gmail.com>
2019-10-11 01:42:06 -0300

    Add Open file function

--- app/public/src/controller/DropBoxController.js
+++ app/public/src/controller/DropBoxController.js
@@ -617,7 +617,7 @@ class DropBoxController {
                     break;
             
                 default:
-                    window.open('/file?path=' + file.path);
+                    window.open(file.path);
                     
             }
 
